943af1c74189d3bf18164b27993161b401546a7b
benhurdobler <benhurdobler1003@gmail.com>
2024-05-14 20:05:39 -0300

    commit_att

--- scripts.js
+++ scripts.js
@@ -4,8 +4,15 @@ function sorteador(){
     const max = Math.floor(parseFloat(document.querySelector(".input-max").value))
     const drawnNumber = document.querySelector(".result")
 
+    if(min >= max){
+        alert("O valor minimo deve ser MENOR que o valor máximo")
+    }else{
+
+
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     drawnNumber.value = result
+
+}
 }
 
